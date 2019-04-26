89de7f6d768c54a94a42d1219865bac7e9270a5c
Ben Askren <ben@buildcalc.com>
2019-04-26 09:16:19 -0400

    Initial commit

---
+++ ShellBackButtonOverride/MyShell.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace ShellBackButtonOverride
+{
+    public partial class MyShell
+    {
+        public MyShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ ShellBackButtonOverride/NextPage.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace ShellBackButtonOverride
+{
+    public partial class NextPage : ContentPage
+    {
+        public NextPage()
+        {
+            InitializeComponent();
+            Shell.SetSetPaddingInsets(this, true);
+
+            Shell.SetBackButtonBehavior(this, new BackButtonBehavior
+            {
+                TextOverride = "Back",
+                Command = new Command((obj) =>
+                {
+                    Navigation.PopAsync();
+                })
+            });
+        }
+    }
+}
